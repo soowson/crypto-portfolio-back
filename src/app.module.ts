289c1a26e055c4fd6f7243fc5338309e2edf07d8
soowson <jakubsowinski94@gmail.com>
2023-04-11 21:06:30 +0200

    connection update

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,7 @@ import { config } from './config/config';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
+      type: "mysql",
       host: config.dbHost,
       port: config.dbPort,
       username: config.dbUsername,
