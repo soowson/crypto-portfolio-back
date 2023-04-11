f90244e6c6d0a3f60c0a12a7cd893270de76292b
soowson <jakubsowinski94@gmail.com>
2023-04-11 21:40:13 +0200

    first commit

--- src/config/config.example.ts
+++ src/config/config.example.ts
@@ -1,8 +1,7 @@
-// export const configExample = {
-//   dbHost: 'localhost',
-//   dbPort: 3306,
-//   dbUsername: 'root',
-//   dbPassword: 'password',
-//   dbDatabase: 'crypto',
-// };
-
+export const config = {
+  dbHost: 'localhost',
+  dbPort: 3306,
+  dbUsername: 'root',
+  dbPassword: 'password',
+  dbDatabase: 'crypto',
+};
