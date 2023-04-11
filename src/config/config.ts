d49d8972a4439b7f31abdbb6dc59a36c9415c39f
soowson <jakubsowinski94@gmail.com>
2023-04-11 21:59:37 +0200

    typeorm

--- src/config/config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-  dbType: 'mysql',
-  dbHost: 'eu-cdbr-west-03.cleardb.net',
-  dbPort: 3306,
-  dbUsername: 'b6f6064b40deb3',
-  dbPassword: '9f6a17d3',
-  dbDatabase: 'heroku_f160f700e0b37b3?reconnect=true',
-};
