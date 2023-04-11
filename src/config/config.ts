9301fce7a771a1fe7138bfc91b4604d4a7d903ff
soowson <jakubsowinski94@gmail.com>
2023-04-11 20:47:23 +0200

    Added db configuration.

---
+++ src/config/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  dbType: 'mysql',
+  dbHost: 'eu-cdbr-west-03.cleardb.net',
+  dbPort: 3306,
+  dbUsername: 'b6f6064b40deb3',
+  dbPassword: '9f6a17d3',
+  dbDatabase: 'heroku_f160f700e0b37b3?reconnect=true',
+};
